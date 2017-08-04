ba47ef8ad6bb82a63ef4ff8443c7f7651e7b52a5
Noturno <Noturno@192.168.7.15>
2017-08-03 21:52:39 -0300

    java

---
+++ Exercicios_Java_Orientacao_Objeto4/src/exercicio5/Data.java
@@ -0,0 +1,34 @@
+package exercicio5;
+
+/*public class Data {
+	private byte dia, mes;
+	private short ano;
+
+	Data(byte d, byte m, short a) {
+		dia = d;
+		mes = m;
+		ano = a;
+	}
+
+	public String toString() {
+		return dia + "/" + mes + "/" + ano;
+	}
+
+}
+*/
+
+public class Data {
+	private byte dia, mes;
+	private short ano;
+
+	Data(byte d, byte m, short a) {
+		dia = d;
+		mes = m;
+		ano = a;
+	}
+
+	public String toString() {
+		return dia + "/" + mes + "/" + ano;
+	}
+
+}
